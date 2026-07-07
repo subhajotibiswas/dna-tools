f9f3984463013022b5dc29b6eebe82c2535f7f2b
Subh <subhajotibiswas.work@gmail.com>
2026-07-07 15:47:40 +0000

    made the type more consistent in transcription & validation

--- tools/validation.py
+++ tools/validation.py
@@ -2,7 +2,7 @@
 
 VALID_BASES = {"A", "T", "G", "C"}
 
-def validate_dna(sequence:str) -> bool:
+def is_valid_dna(sequence: str) -> bool:
     for base in sequence:
         if base not in VALID_BASES:
             return False
